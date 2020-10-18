8afd703cb5bc574b0cde1268a0e746fb0d6da702
WALIII <bliberti@bu.edu>
2020-10-18 11:04:53 -0700

    show images

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # LungDamage
 Quantify tissue damage with ML
+
+
+![ScreenShot](images/Workflow-01.png)
--- XMASS_tish.m
+++ XMASS_tish.m
@@ -9,7 +9,8 @@ if nargin < 4
 elseif nargin < 6
    HL = [0.5 .95];
 end
-  HL = [0.05 .95];
+
+  HL = [0.50 .95];
 Llim = HL(1);
  Hlim = HL(2);
 
