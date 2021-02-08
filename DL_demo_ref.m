59b37a192b1232a475e364df4736dede87a8c1de
WALIII <bliberti@bu.edu>
2021-02-08 13:21:02 -0800

    fix typo in comments

--- DL_demo_ref.m
+++ DL_demo_ref.m
@@ -15,7 +15,7 @@ function out = DL_demo_ref(reference,varargin)
 %    PcentR - Percent of severly damaged tissue
 %    PcentG - Percent of moderatly damaged tissue
 %    PcentG - Percent of normal tissue
-%    RGB_out = RGB image for each tissue image 
+%    RGB_out = RGB image for each tissue image
 %
 % Example:
 %    out = DL_demo_ref('S copy.jpg')
@@ -65,8 +65,8 @@ horz_save(1) = size(RefIm,2); % get size reference
 for i=1:length(filenames)
     RGB2 = imread(filenames{i});
     RGB2 = imresize(RGB2,size(RefIm,1)./size(RGB2,1));
-    
-    % concatonate Images
+
+    % concatenate images
     if i ==1;
         GG = cat(2,RefIm,RGB2);
     else
@@ -93,17 +93,17 @@ RGB3 = imgaussfilt(RGB1,sigma,'padding','circular');
 
 A1 = [1,horz_save];%1: ((size(RGB2,2)-1)/3): size(RGB2,2); % breakdown size of im matrix
 for i = 1:(length(filenames)+1);
-    
+
     [HH1 HH2] = max(RGB3,[],3);
-    
+
     HH2(mean(RGB2(:,:,:),3)<0.001) = NaN;
-    
+
     RIm{i} = HH2(:,A1(i):A1(i+1),1);
     GIm{i} = HH2(:,A1(i):A1(i+1),1);
     BIm{i} = HH2(:,A1(i):A1(i+1),1);
-    
+
     RGB_out{i} = RGB2(:,A1(i):A1(i+1),:);
-    
+
     RIm2{i} = sum(find(RIm{i}==1));
     GIm2{i} = sum(find(GIm{i}==2));
     BIm2{i} = sum(find(BIm{i}==3));
@@ -111,9 +111,9 @@ for i = 1:(length(filenames)+1);
     PcentG(i) = GIm2{i}/(RIm2{i}+GIm2{i}+BIm2{i});
     PcentB(i) = BIm2{i}/(RIm2{i}+GIm2{i}+BIm2{i});
     data(:,i) = [PcentB(i),PcentG(i),PcentR(i)];
-    
+
     % Create similarity Score to reference ( first ) image:
-    
+
 end
 
 figure();
@@ -139,11 +139,10 @@ mkdir('clustered_images');
 for i = 1:length(RGB_out);
     if i ==1
         imwrite(RGB_out{i},['clustered_images/',reference(1:end-4),'_clustered.tif'])
-        
+
     else
         imwrite(RGB_out{i},['clustered_images/',filenames{i-1}(1:end-4),'_clustered.tif'])
     end
 end
 DL_display_image(GG,out_mat);
 saveas(gcf, ['clustered_images/','Bounded_Images','_clustered.tif'])
-
